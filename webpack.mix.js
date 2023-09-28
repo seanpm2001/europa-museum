c995d196e1448f0f90d2e60e94b1f2385fb7ba5b
Tim Kelty <tim@craftcms.com>
2023-09-28 15:14:49 -0400

    publicPath

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,12 @@ require('laravel-mix-criticalcss');
 require('laravel-mix-purgecss');
 
 mix
-    .setPublicPath('./web/dist')
+    .setPublicPath(`${process.env.CRAFT_CLOUD_ARTIFACT_BASE_URL}/dist/`)
     .sass('src/css/site.scss', './web/dist/css')
     .js('src/js/site.js', './web/dist/js')
     .copy('src/images', './web/dist/images')
     .copy('src/fonts', './web/dist/fonts')
-    // .copy('src/favicon.ico', './web/dist')
+    .copy('src/favicon.ico', './web/dist')
 
     .options({
         autoprefixer: false,
@@ -32,25 +32,25 @@ mix
         whitelist: ['first-load', 'loading', 'is-ie', 'is-device', 'is-phone', 'is-desktop', 'lazypicture', 'lazyloading', 'lazyloaded', 'lazyfade', 'lazyblur', 'animated-gif', 'has-scroll-smooth', 'has-scroll-init', 'has-scroll-scrolling', 'has-scroll-dragging', 'c-scrollbar', 'c-scrollbar_thumb', 'is-inview', 'flickity-button', 'flickity-button-icon', 'previous', 'next', 'arrow', 'flickity-page-dots', 'is-selected', 'scrolled', 'page-404', 'page-503', 'page-error', '[data-router-wrapper]', '[data-page="home"]', '[data-page="exhibitions"]', '[data-page="visit"]', '[data-page="news"]', '[data-page="about"]', '[data-page="contact"]', '[data-router-view="exhibitions"]', '[data-router-view="visit"]', '[data-router-view="news"]', '[data-router-view="newsArticle"]', '[data-router-view="about"]', '[data-router-view="contact"]', '[data-router-view="styleguide"]', 'active', 'word', 'whiteText', 'exhibit-odd', 'exhibit-even', 'exhibit-1', 'exhibit-2', 'exhibit-3', 'exhibit-4', 'dark-ui', 'has-icon', 'no-blocks', 'isFirst', 'is-image', 'is-entry', 'is-highlighted', 'topBorder', 'layout-imageLeft', 'layout-imageFullWidth', 'layout-imageRight', 'narrowWidth', 'no-hero-image', 'figure']
     })
 
-    // .criticalCss({
-    //     enabled: mix.inProduction(),
-    //     paths: {
-    //         base: process.env.DEFAULT_SITE_URL,
-    //         templates: './templates/_/',
-    //         suffix: '-critical.min'
-    //     },
-    //     urls: [
-    //         {
-    //             url: '/',
-    //             template: 'home'
-    //         },
-    //     ],
-    //     options: {
-    //         minify: true,
-    //         width: 1440,
-    //         height: 1200,
-    //     },
-    // })
+    .criticalCss({
+        enabled: mix.inProduction(),
+        paths: {
+            base: 'https://spokeandchain.co/,
+            templates: './templates/_/',
+            suffix: '-critical.min'
+        },
+        urls: [
+            {
+                url: '/',
+                template: 'home'
+            },
+        ],
+        options: {
+            minify: true,
+            width: 1440,
+            height: 1200,
+        },
+    })
 
     .babelConfig({
         plugins: [
@@ -70,6 +70,7 @@ if (mix.inProduction()) {
             publicPath: `${process.env.CRAFT_CLOUD_ARTIFACT_BASE_URL}/dist/`,
         }
     })
+    mix.setResourceRoot(`${process.env.CRAFT_CLOUD_ARTIFACT_BASE_URL}/dist/`);
 } else if (process.env.MIX_ENV == 'sync') {
     mix
         .sourceMaps(true, 'source-map')
